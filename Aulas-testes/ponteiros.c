f33560ba77cffef10ad40d44526814583c330c52
Ricardo Victor <ricardovictor737@gmail.com>
2023-08-08 20:00:31 -0300

    questao1

--- Aulas-testes/ponteiros.c
+++ Aulas-testes/ponteiros.c
@@ -19,9 +19,61 @@ int main(){
 	struct horario agora, *depois;
 	depois = &agora;
 	
-	depois->.hora = 20; //Em caso de estruturas, colocamos o ponteiro entre parenteses quando trocamos um valor dentro(*depois) ou apenas usamos ->
-	depois->.minuto = 55;
-	depois->.segundos = 15;
+	depois->hora = 20; //Em caso de estruturas, colocamos o ponteiro entre parenteses quando trocamos um valor dentro(*depois) ou apenas usamos ->
+	depois->minuto = 55;
+	depois->segundo = 15;
 
-	printf("%d:%d:%d", agora.hora, agora.minuto, agora.segundo);
+	int soma = 100;
+	
+	struct horario antes;
+	antes.hora = soma + depois->segundo;
+	
+		
+	//printf("%d:%d:%d", agora.hora, agora.minuto, agora.segundo);
+	
+	//printf("\n%d", antes.hora);
+	
+	
+	//Agora uma struct que possui um ponteiro
+	
+	struct horario2{
+		int *hora;
+		int *minuto;
+		int *segundo;
+	};
+	struct horario2 now;
+	int horaa = 100;
+	int minut = 40;
+	int segund = 33;
+	
+	now.hora = &horaa;
+	now.minuto = &minut;
+	now.segundo = &segund;
+	
+	//printf("\n%d:%d:%d", *now.hora, *now.minuto, *now.segundo);
+	
+	
+	
+	//Ponteiros em listas
+	
+	struct lista{
+		int valor;
+		struct lista *proximo;
+	};	
+	struct lista m1, m2, m3;
+	struct lista *gancho = &m1;
+	
+	m1.valor = 10;
+	m2.valor = 20;
+	m3.valor = 30;
+	
+	m1.proximo = &m2;
+	m2.proximo = &m3;
+	m3.proximo = (struct lista *)0;
+	
+	while(gancho != (struct lista *)0){
+		printf("%d\n", gancho->valor);
+		gancho = gancho->proximo;
+	}
+		
 }
